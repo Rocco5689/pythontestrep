5106f5fb9854309968751a99e5fa9cade972b6b8
Rocco5689 <58642500+Rocco5689@users.noreply.github.com>
2024-05-14 16:53:05 -0500

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -20,6 +20,6 @@ def http2(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "Hello THERE!!!.",
              status_code=200
         )
